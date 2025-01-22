41c5ddbc176aef82485782a2606367ab902e25a4
leandroW <leandro_antunes28@hotmail.com>
2025-01-22 20:28:20 -0300

    criacao tela de cadastro

--- app/src/main/java/com/tunesapp/tunesbank/activities/FormCadastro/FormCadastro.kt
+++ app/src/main/java/com/tunesapp/tunesbank/activities/FormCadastro/FormCadastro.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.tunesapp.tunesbank.R
+import com.tunesapp.tunesbank.databinding.ActivityFormCadastroBinding
 
 class FormCadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: ActivityFormCadastroBinding
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_form_cadastro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityFormCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
